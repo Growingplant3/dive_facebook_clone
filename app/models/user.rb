923a1762dc0c7ea005ce66a0acebbcfaa36113fc
下田　佳紀 <y.s@platinum.local>
2020-11-21 00:45:28 +0900

    Userモデルのname/emailカラムにバリデーションを付与

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  before_validation { email.downcase! }
+  validates :name,
+    presence: true,
+    length: { maximum: 20 }
+  validates :email,
+    presence: true,
+    length: { maximum: 50 },
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+    uniqueness: true
 end

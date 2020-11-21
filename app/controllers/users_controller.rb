801fe82d48bc3753c19a2bd84c6a5ce739063bf5
下田　佳紀 <y.s@platinum.local>
2020-11-21 13:58:47 +0900

    showアクション整備

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show]
+
   def new
     @user = User.new
   end
@@ -6,13 +8,21 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      redirect_to user_path(@user)
     else
       render :new
     end
   end
 
+  def show
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end

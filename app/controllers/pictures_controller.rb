e86bfbbe3681545a4012856cfe2a7bc1f9730dc5
下田　佳紀 <y.s@platinum.local>
2020-11-23 17:05:10 +0900

    confirmアクション整備

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -43,6 +43,8 @@ class PicturesController < ApplicationController
   end
 
   def confirm
+    @picture = current_user.pictures.build(picture_params)
+    render :new if @picture_params.invalid?
   end
 
   private

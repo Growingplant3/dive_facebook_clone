1c2b1562c52109eec5275b5cfaf498144babeff7
下田　佳紀 <y.s@platinum.local>
2020-11-23 13:51:31 +0900

    new/create/indexアクションを整備

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,8 +1,28 @@
 class PicturesController < ApplicationController
-  def new
-    @picture = Picture.new
+  def index
+    @pictures = Picture.all.order(created_at: "desc")
   end
-  
+
+  def new
+    @picture = current_user.pictures.build
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      flash[:notice] = "新規登録に成功しました。"
+      redirect_to pictures_path
+    else
+      flash[:alert] = "新規登録に失敗しました。"
+      render :new
+    end
+  end
+
   def confirm
+  end
+
+  private
+  def picture_params
+    params.require(:picture).permit(:content, :image)
   end
 end

fe3c1a4e2f9f4948068f9ddaf9bbf81dface53ef
下田　佳紀 <y.s@platinum.local>
2020-11-23 14:47:58 +0900

    create後の遷移先変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "アカウントを作成しました。"
-      redirect_to user_path(@user)
+      redirect_to new_session_path
     else
       render :new
     end

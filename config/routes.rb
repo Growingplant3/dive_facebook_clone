fb6af7294ef447d736a4c6bd97c62cb4a1069dc7
下田　佳紀 <y.s@platinum.local>
2020-11-23 11:34:24 +0900

    pictureのルート整備

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: %i[new create show destroy]
   resources :sessions, only: %i[new create destroy]
+  resources :pictures do
+    post :confirm, on: :collection
+  end
 end

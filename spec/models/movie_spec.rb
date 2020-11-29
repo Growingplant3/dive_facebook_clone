7b8579ba4d7c09d0581af7e2f8c4db6ab57e979c
下田　佳紀 <y.s@platinum.local>
2020-11-29 17:24:05 +0900

    テストコードの演習

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -11,9 +11,33 @@ RSpec.describe Movie, type: :model do
     expect(movie).to be_valid
   end
   # タイトルがなければ無効な状態であること
-  it "is invalid without a title"
+  it "is invalid without a title" do
+    movie = Movie.new(title: nil)
+    movie.valid?
+    # expect(movie.errors[:title]).to include("can't be blank")
+    expect(movie.errors[:title]).to include("を入力してください")
+  end
   # 重複したタイトルなら無効な状態であること
-  it "is invalid with a duplicate title"
+  it "is invalid with a duplicate title" do
+    Movie.create(
+      title: "DIC",
+      production_year: 2000,
+      running_time: 120,
+      rating: "G"
+    )
+    movie = Movie.new(
+      title: "DIC",
+      production_year: 3000,
+      running_time: 150,
+      rating: "PG12"
+    )
+    movie.valid?
+    expect(movie.errors[:title]).to include("はすでに存在します")
+  end
   # タイトルが101文字以上なら無効な状態であること
-  it "is invalid with title is 101 or more characters"
+  it "is invalid with title is 101 or more characters" do
+    movie = Movie.new(title: "a" * 101)
+    movie.valid?
+    expect(movie.errors[:title]).to include("は100文字以内で入力してください")
+  end
 end

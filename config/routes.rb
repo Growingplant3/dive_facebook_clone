3c2c52865104c6243d213518c245fa3342fe75ac
下田　佳紀 <y.s@platinum.local>
2020-12-16 11:10:37 +0900

    新たなルート作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create show destroy]
   resources :sessions, only: %i[new create destroy]
   resources :pictures do
+    resources :comments
     post :confirm, on: :collection
   end
   root to: 'sessions#new'

c9dabdf60e16b92451e37ef49c22665813da8ab3
下田　佳紀 <y.s@platinum.local>
2020-12-16 19:22:46 +0900

    indexへのルート整備

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :users, only: %i[new create show destroy]
+  resources :users, only: %i[index new create show destroy]
   resources :sessions, only: %i[new create destroy]
   resources :pictures do
     resources :comments
     post :confirm, on: :collection
   end
+  resources :relationships, only: %i[create destroy]
   root to: 'sessions#new'
 end

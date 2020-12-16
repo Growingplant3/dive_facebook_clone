c3f3dce4ac736ed51db38946bed7105e2afe3bcb
下田　佳紀 <y.s@platinum.local>
2020-12-16 11:11:50 +0900

    マイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_075348) do
+ActiveRecord::Schema.define(version: 2020_12_16_020413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "picture_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["picture_id"], name: "index_comments_on_picture_id"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -42,5 +50,6 @@ ActiveRecord::Schema.define(version: 2020_11_29_075348) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "comments", "pictures"
   add_foreign_key "pictures", "users"
 end
